3db74113f470c29638267d0e7219ca1172c9bfe1
unknown <rishabdutt4@gmail.com>
2026-05-11 12:47:10 +0530

    11may2026

--- src/App.js
+++ src/App.js
@@ -1,27 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-// import { Routes, Route } from "react-router-dom";
+
+import { motion } from "framer-motion";
+
 import Navbar from "./components/Navbar";
-// import FloatingButtons from "./components/FloatingButtons";
 import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
+
+import preloadDish from "./assets/pre-loader.png";
+
 import "./App.css";
 
-
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-   <div className="App">
-      <Navbar />
+    <>
+      {loading ? (
+        <div className="preloader">
 
-      {/* ONLY HOME PAGE */}
-      <Home />
+          {/* DISH IMAGE */}
+          <div className="dish-image-wrapper">
 
-      <Footer />
-    </div>
+            {/* STEAM */}
+            <span className="steam steam1"></span>
+            <span className="steam steam2"></span>
+            <span className="steam steam3"></span>
+
+            {/* IMAGE */}
+            <motion.img
+              src={preloadDish}
+              alt="Khasiyat Dish"
+              className="preloader-dish-img"
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 1 }}
+            />
+          </div>
+
+          {/* BRAND NAME */}
+          <motion.h1
+            className="brand-name"
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3, duration: 1 }}
+          >
+            KHASIYAT
+          </motion.h1>
+
+          {/* TAGLINE */}
+          <motion.p
+            className="brand-tag"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.8, duration: 1 }}
+          >
+          Where Every Bite & Sip Feels Special
+          </motion.p>
+        </div>
+      ) : (
+        <div className="App">
+          <Navbar />
+
+          {/* HOME PAGE */}
+          <Home />
+
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
