bef946058de95e88fc8d93dff7ba1fce289b1371
rishab-sudo <rishabdutt4@gmail.com>
2026-06-11 16:50:08 +0530

    11june26

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -8,26 +8,40 @@ const Staff = () => {
 
         {/* LEFT COLOR BLOCK */}
         <div className="staff-left">
-          <div className="staff-color-box">
-           <span className="staff-tag">
-            PURE VEG RESTAURANT IN PAHALGAM
+          <div className="staff-color-box ">
+           <span className="staff-tag page-heading  ">
+           Our Story & Vision
+
           </span>
 
-          <h2 className="staff-title page-heading">
+          {/* <h2 className="staff-title page-heading">
       The Story Behind Khaasiyat
 
-          </h2>
+          </h2> */}
 
           <p className="staff-description page-description">
-          Founded by Harish Kale, Khaasiyat was created with a vision to offer travellers and families a premium pure vegetarian dining experience in
-           the heart of Pahalgam. Inspired by the challenge of finding quality vegetarian food during family travels, the restaurant was established to celebrate authentic flavours, warm hospitality, and memorable dining experiences, brought to life by our skilled chefs who craft every dish with passion, care, and attention to detail.
+          Founded by Harish Kale, Khaasiyat was born from a vision to create a welcoming pure vegetarian dining destination for travellers and families visiting Pahalgam.
           </p>
+<div>
 
-         
+   <p className="page-description" style={{color: "#f4c542"}}> Crafted By Skilled Chefs</p>
+   <p className="page-description">Every dish is thoughtfully prepared by experienced chefs using quality ingredients, authentic recipes, and a passion for exceptional flavour.</p>
+</div>
+  
+  <div>
 
-          <p className="staff-highlight">
-            Fresh ingredients. Soulful flavours. Warm hospitality.
-          </p>
+   <p className="page-description" style={{color: "#f4c542"}}>Pure Veg & Jain Friendly</p>
+   <p className="page-description">Enjoy a diverse multi-cuisine menu featuring North Indian, South Indian, Chinese, Tandoor specialties, and specially prepared Jain food options.
+</p>
+</div>
+  
+
+  <div>
+
+   <p className="page-description" style={{color: "#f4c542"}}>Warm Hospitality & Scenic Dining</p>
+   <p className="page-description"> From attentive service to beautiful surroundings, every experience at Khaasiyat is designed to make guests feel comfortable, valued, and at home.</p>
+</div>
+  
           </div>
         </div>
 
